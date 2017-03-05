4669cac1a5fb5c77bb889399097096f17632a51a
richelbilderbeek <richel@richelbilderbeek.nl>
2017-03-05 10:08:06 +0100

    Desktop compiles locally

--- qtmain.cpp
+++ qtmain.cpp
@@ -1,7 +1,5 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Weffc++"
-#include <cassert>
-#include <iostream>
 #include <QApplication>
 #include "qtrubiksclockmenudialog.h"
 
@@ -10,14 +8,6 @@
 int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
-
-  #ifndef NDEBUG
-  std::clog << "DEBUG mode" << std::endl;
-  #else
-  std::clog << "RELEASE mode" << std::endl;
-  assert(1==2 && "Assume debugging is really disabled");
-  #endif
-
   ribi::ruco::QtRubiksClockMenuDialog d;
   d.show();
   return a.exec();
--- qtrubiksclockmenudialog.cpp
+++ qtrubiksclockmenudialog.cpp
@@ -1,24 +1,3 @@
-//---------------------------------------------------------------------------
-/*
-RubiksClock. Rubik's Clock game.
-Copyright (C) 2007-2015 Richel Bilderbeek
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-//---------------------------------------------------------------------------
-//From http://www.richelbilderbeek.nl/GameRubiksClock.htm
-//---------------------------------------------------------------------------
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Weffc++"
 #include "qtrubiksclockmenudialog.h"
@@ -32,8 +11,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "qtrubiksclockwidget.h"
 #include "qttogglebuttonwidget.h"
 #include "rubiksclockmenudialog.h"
-#include "testtimer.h"
-#include "trace.h"
 #include "ui_qtrubiksclockmenudialog.h"
 #pragma GCC diagnostic pop
 
