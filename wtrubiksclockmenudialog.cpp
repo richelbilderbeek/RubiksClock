c2162a852a251e2f0fbb1f3e3f479f58c85cad14
richelbilderbeek <richel@richelbilderbeek.nl>
2019-08-03 14:12:06 +0200

    Remove pragma's

--- wtrubiksclockmenudialog.cpp
+++ wtrubiksclockmenudialog.cpp
@@ -26,7 +26,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma GCC diagnostic push
 
 
-#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
+
 
 #include <Wt/WBreak>
 #include <Wt/WContainerWidget>
